1b6ff1a033c80acf78b26538611591b67c359f04
Gopi-Git-Hub1 <01ch.gopi@gmail.com>
2022-07-19 01:33:04 +0530

    printf function corrected in sum_of_two_integers.c file

--- sum_of_two_integers.c
+++ sum_of_two_integers.c
@@ -2,9 +2,9 @@
 int main()
 {
 	int num1,num2,sum;
-	printf("Enter the Num1 : );
+	printf("Enter the Num1 : ");
 	scanf("%d",&num1);
-	printf("Enter the Num2 : );
+	printf("Enter the Num2 : ");
 	scanf("%d",&num2);
 	sum = num1 + num2;
 	printf("sum of %d and %d is : %d\n",num1,num2,sum);
