4b244435191f5d97f9ac649abd03bbff7f8468a1
Gopi-Git-Hub1 <01ch.gopi@gmail.com>
2022-07-19 00:49:20 +0530

    sum of two integers

---
+++ Sum_of_two_integers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum;
+	printf("Enter the Num1 : );
+	scanf("%d",&num1);
+	printf("Enter the Num2 : );
+	scanf("%d",&num2);
+	sum = num1 + num2;
+	printf("sum of %d and %d is : %d\n",num1,num2,sum);
+	return 0;
+}
